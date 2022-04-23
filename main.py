deb724ab596e0ffa217b88970c4efbacffdb8a91
Ashlyn <ashtree.ng@gmail.com>
2022-04-24 02:10:40 +1000

    Modified background colour, tried to change resource, made initial snake length=1

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import time
 import random
 import logging
 
+
 class Apple:
     def __init__(self, parent_screen):
         self.parent_screen = parent_screen
@@ -80,7 +81,7 @@ class Snake:
 
 
 class Game:
-    #change length of snake to 1 so score can be 1
+    # change length of snake to 1 so score can be 1
     def __init__(self):
         pygame.init()
         self.surface = pygame.display.set_mode((X_SIZE, Y_SIZE))
@@ -108,11 +109,10 @@ class Game:
             logging.debug("moving down")
             self.snake.move_down()
             return
-        if self.snake.direction =='down':
+        if self.snake.direction == 'down':
             logging.debug("moving up")
             self.snake.move_up()
             return
-
 
     def check_boundary(self, x1, y1, OUTSIDE):
         if (x1 <= 0 or x1 > X_SIZE) and OUTSIDE == 0:
@@ -130,13 +130,13 @@ class Game:
             logging.debug("Resetting OUTSIDE for Y")
             OUTSIDE = 0
         return OUTSIDE
-#scoreboard        
+# scoreboard
+
     def display_Score(self):
         font = pygame.font.SysFont('arial', 30)
         score = font.render(
             f"Score: {self.snake.length}", True, (200, 200, 200))
         self.surface.blit(score, (850, 10))
-        
 
     def play(self):
         self.snake.walk()
@@ -148,13 +148,16 @@ class Game:
             logging.debug("COLLISION, snake[%s, %s], apple[%s, %s], snake before length[%s]",
                           self.snake.x[0], self.snake.y[0], self.apple.x, self.apple.y, self.snake.length)
             self.snake.increase_length()
-            logging.debug("After increase length, snake new length[%s]", self.snake.length)
-            logging.debug("BEFORE apple move, apple[%s, %s]", self.apple.x, self.apple.y)
+            logging.debug(
+                "After increase length, snake new length[%s]", self.snake.length)
+            logging.debug(
+                "BEFORE apple move, apple[%s, %s]", self.apple.x, self.apple.y)
             self.apple.move()
-            logging.debug("AFTER apple move, apple[%s, %s]", self.apple.x, self.apple.y)
+            logging.debug(
+                "AFTER apple move, apple[%s, %s]", self.apple.x, self.apple.y)
         else:
-            logging.debug("No collision, snake[%s, %s], apple[%s, %s]", self.snake.x[0], self.snake.y[0], self.apple.x, self.apple.y)
-
+            logging.debug("No collision, snake[%s, %s], apple[%s, %s]",
+                          self.snake.x[0], self.snake.y[0], self.apple.x, self.apple.y)
 
     def run(self):
         running = True
@@ -180,7 +183,8 @@ class Game:
 
                 elif event.type == QUIT:
                     running = False
-            OUTSIDE = self.check_boundary(self.snake.x[0], self.snake.y[0], OUTSIDE)
+            OUTSIDE = self.check_boundary(
+                self.snake.x[0], self.snake.y[0], OUTSIDE)
             logging.debug("Value of OUTSIDE[%s]", OUTSIDE)
             self.play()
 
@@ -190,7 +194,8 @@ class Game:
 if __name__ == "__main__":
     # Change the logging level below from DEBUG to INFO when you are finished
     # Please remove game.log each time you run, or you will fill up your disc!
-    logging.basicConfig(filename='game.log', encoding='utf-8', level=logging.DEBUG)
+    logging.basicConfig(filename='game.log',
+                        encoding='utf-8', level=logging.DEBUG)
     logging.debug('This message should go to the log file')
     SIZE = 40
     X_SIZE = 1000
